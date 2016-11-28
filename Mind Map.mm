42390d6cf7865a720cc76a83f0e38624ee0c7e32
theomatthews95 <theo.matthews@hotmail.co.uk>
2016-11-28 19:55:19 +0000

    First Commit

---
+++ Mind Map.mm
@@ -0,0 +1,146 @@
+<map version="freeplane 1.5.9">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="myGRaCE Safety Assessment" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1573594237" CREATED="1462710693327" MODIFIED="1480352971308" TEXT_ALIGN="CENTER" MAX_WIDTH="260.0 pt" MIN_WIDTH="182.0 pt">
+<font SIZE="16"/>
+<hook NAME="MapStyle" background="#f7f7f7">
+    <properties fit_to_viewport="false;" show_icon_for_attributes="true" show_note_icons="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt" TEXT_SHORTENED="true">
+<font SIZE="24"/>
+<richcontent TYPE="DETAILS" LOCALIZED_HTML="styles_background_html"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<font SIZE="9"/>
+<stylenode LOCALIZED_TEXT="default" COLOR="#000000" STYLE="bubble" SHAPE_VERTICAL_MARGIN="0.0 pt" TEXT_ALIGN="CENTER" MAX_WIDTH="120.0 pt" MIN_WIDTH="120.0 pt">
+<font NAME="Arial" SIZE="9" BOLD="false" ITALIC="false"/>
+<edge STYLE="bezier" WIDTH="1"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes" COLOR="#000000" BACKGROUND_COLOR="#cccccc" MAX_WIDTH="260.0 pt" MIN_WIDTH="260.0 pt" VGAP_QUANTITY="5.0 pt">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<font SIZE="9"/>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+<edge COLOR="#0000cc"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<font SIZE="9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" STYLE="fork" UNIFORM_SHAPE="true" TEXT_ALIGN="CENTER" MAX_WIDTH="120.0 pt" MIN_WIDTH="100.0 pt" VGAP_QUANTITY="10.0 pt">
+<font SIZE="16"/>
+<edge STYLE="bezier" WIDTH="3"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" NUMBERED="true" MIN_WIDTH="100.0 pt" VGAP_QUANTITY="5.0 pt">
+<font SIZE="11" BOLD="true"/>
+<edge COLOR="#a3a3ff" WIDTH="1"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" MAX_WIDTH="200.0 pt" MIN_WIDTH="20.0 pt" VGAP_QUANTITY="5.0 pt" TEXT_ALIGN="LEFT">
+<edge COLOR="#ff77b3" WIDTH="1"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" MAX_WIDTH="260.0 pt" MIN_WIDTH="20.0 pt" TEXT_ALIGN="LEFT" VGAP_QUANTITY="5.0 pt">
+<edge COLOR="#ff9933" WIDTH="1"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" MAX_WIDTH="260.0 pt" MIN_WIDTH="20.0 pt" TEXT_ALIGN="LEFT" VGAP_QUANTITY="5.0 pt">
+<edge COLOR="#9999ff" WIDTH="1"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5">
+<edge COLOR="#968900"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6">
+<edge COLOR="#cc00cc"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7">
+<edge COLOR="#ffbf00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8">
+<edge COLOR="#00ff99"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9">
+<edge COLOR="#0099ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10">
+<edge COLOR="#996600"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11">
+<edge COLOR="#000000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,12">
+<edge COLOR="#cc0066"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,13">
+<edge COLOR="#33ff00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,14">
+<edge COLOR="#ff9999"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,15">
+<edge COLOR="#0000cc"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,16">
+<edge COLOR="#cccc00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,17">
+<edge COLOR="#0099cc"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,18">
+<edge COLOR="#006600"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,19">
+<edge COLOR="#ff00cc"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,20">
+<edge COLOR="#00cc00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,21">
+<edge COLOR="#0066cc"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,22">
+<edge COLOR="#00ffff"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="0" RULE="FOR_BRANCHES"/>
+<edge COLOR="#000000"/>
+<node TEXT="Functional" POSITION="right" ID="ID_978846425" CREATED="1480347288807" MODIFIED="1480352983262">
+<node TEXT="Perform &apos;My Safety&apos; test" ID="ID_865554448" CREATED="1480347288807" MODIFIED="1480353508428">
+<node TEXT="Store question tree locally" ID="ID_1529982344" CREATED="1480347288807" MODIFIED="1480353543055"/>
+<node TEXT="Store user response in JSON" ID="ID_427804668" CREATED="1480353549986" MODIFIED="1480353575978"/>
+<node TEXT="Present each question on individual page" ID="ID_1079946497" CREATED="1480361409689" MODIFIED="1480361430051"/>
+</node>
+<node TEXT="User can create" ID="ID_291945426" CREATED="1480353516877" MODIFIED="1480361447444">
+<node TEXT="Risk formulation" ID="ID_188547802" CREATED="1480361448558" MODIFIED="1480361458911"/>
+<node TEXT="Safety plan" ID="ID_276328088" CREATED="1480361466372" MODIFIED="1480361471589"/>
+</node>
+<node TEXT="Connection to GRiST" ID="ID_988686326" CREATED="1480361519762" MODIFIED="1480361550683">
+<node TEXT="User login" ID="ID_108586833" CREATED="1480361557006" MODIFIED="1480361563898"/>
+<node TEXT="Submit assessments to profile" ID="ID_324436209" CREATED="1480361565484" MODIFIED="1480361580323"/>
+</node>
+</node>
+<node TEXT="Non-functional" POSITION="left" ID="ID_831116123" CREATED="1480352994490" MODIFIED="1480352999832">
+<node TEXT="User interface" ID="ID_1861522753" CREATED="1480361593194" MODIFIED="1480361637179">
+<node TEXT="1-10 scales" ID="ID_1451865986" CREATED="1480361647276" MODIFIED="1480361825988">
+<node TEXT="Colour to indicate risk" ID="ID_1850161430" CREATED="1480361828049" MODIFIED="1480361835913"/>
+<node TEXT="&apos;i&apos; button to provide extra information to scale" ID="ID_576456810" CREATED="1480361836539" MODIFIED="1480361850022"/>
+</node>
+<node TEXT="Test progress" ID="ID_1174334716" CREATED="1480361860100" MODIFIED="1480361873658">
+<node TEXT="Allow user to skip to specific test sections" ID="ID_1990111069" CREATED="1480361876841" MODIFIED="1480361889046"/>
+<node TEXT="Provide test progress bar" ID="ID_23247369" CREATED="1480361889791" MODIFIED="1480361939262"/>
+</node>
+<node TEXT="Usability" ID="ID_1606201997" CREATED="1480361943582" MODIFIED="1480361952246">
+<node TEXT="Provide hints and tips for first time users" ID="ID_1699553673" CREATED="1480361955021" MODIFIED="1480361982376"/>
+</node>
+</node>
+</node>
+</node>
+</map>
